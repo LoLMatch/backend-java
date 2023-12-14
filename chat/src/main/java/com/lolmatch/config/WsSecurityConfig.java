d4e4ba36e1fb3cfbb15634cdd6f0777d1b553032
Wojciech <wojjmaj22@gmail.com>
2023-12-14 17:47:43 +0100

    added Dockerfile and logs

--- chat/src/main/java/com/lolmatch/config/WsSecurityConfig.java
+++ chat/src/main/java/com/lolmatch/config/WsSecurityConfig.java
@@ -1,14 +1,9 @@
 package com.lolmatch.config;
 
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.Message;
-import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
-import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
-import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
 
 @Configuration
 public class WsSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
--- chat/src/main/java/com/lolmatch/controller/MessageController.java
+++ chat/src/main/java/com/lolmatch/controller/MessageController.java
@@ -3,14 +3,15 @@ package com.lolmatch.controller;
 import com.lolmatch.dto.FetchMessagesDTO;
 import com.lolmatch.service.MessageService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class MessageController {
@@ -24,6 +25,7 @@ public class MessageController {
 			@RequestParam("size") Optional<Integer> size
 	){
 		// TODO - this method will return list of messages according to search criteria from request params
+		log.info("Get messages request, details: senderId-" + senderId + ";recipientId-" + recipientId + ";size" + size);
 		return messageService.getListOfMessages(senderId, recipientId, size);
 	}
 }
