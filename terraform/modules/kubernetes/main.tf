362fb724c3fcbafc54259aa0ce96e2cf037c90f2
KubaKrzych <krzychowiec.k@gmail.com>
2023-12-28 10:32:06 +0100

    Fix deletion_protection

--- terraform/modules/kubernetes/main.tf
+++ terraform/modules/kubernetes/main.tf
@@ -15,6 +15,8 @@ resource "google_container_cluster" "primary" {
 
   remove_default_node_pool = true
   initial_node_count       = 1
+  deletion_protection      = false
+
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
