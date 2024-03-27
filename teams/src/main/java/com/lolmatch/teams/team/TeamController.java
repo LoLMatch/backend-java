186c8f0e523d9ab6c6c7653c414e20e579a8421e
Wojciech <wojjmaj22@gmail.com>
2024-03-27 21:43:27 +0100

    changed paths

--- teams/src/main/java/com/lolmatch/teams/team/TeamController.java
+++ teams/src/main/java/com/lolmatch/teams/team/TeamController.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 
 @Slf4j
 @RestController
-@RequestMapping("/teams/api/teams")
+@RequestMapping("/teams")
 @RequiredArgsConstructor
 public class TeamController {
 	
--- teams/src/main/java/com/lolmatch/teams/user/UserController.java
+++ teams/src/main/java/com/lolmatch/teams/user/UserController.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/teams/api/users")
+@RequestMapping("/users")
 public class UserController {
 	private final UserService userService;
 	
