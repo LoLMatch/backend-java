9530d16171f3813c02a7e01e5f81d277d0143e91
Wojciech <wojjmaj22@gmail.com>
2024-04-21 12:55:31 +0200

    hotfix

--- teams/src/main/java/com/lolmatch/teams/TestDataInitializer.java
+++ teams/src/main/java/com/lolmatch/teams/TestDataInitializer.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
-@Profile({"local","dev"})
+@Profile({"local","docker"})
 public class TestDataInitializer {
 	
 	private final UserRepository userRepository;
