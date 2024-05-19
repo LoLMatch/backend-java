07e251954bed3417d22bd2bf46936fe6103abe96
KubaKrzych <krzychowiec.k@gmail.com>
2024-05-19 23:05:17 +0200

    poprawiony gateway, dodany healthcheck

---
+++ api-gateway/src/main/java/com/lolmatch/apigateway/HealthCheckController.java
@@ -0,0 +1,11 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/healthcheck")
+    public int healthCheck() {
+        return 0; // zawsze zwraca 0
+    }
+}
